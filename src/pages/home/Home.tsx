2dac683c55e3bb1ac3d58f43207ba307041bde81
Yulia <linguavoice1@yahoo.com>
2025-05-07 17:23:43 +0300

    fix: remove help center section from the Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,11 @@
 import Media from '../../components/layout/media/Media';
-import Help from '../../components/layout/help/Help';
 import Footer from '../../components/layout/footer/Footer';
 
 export const HomePage = () => {
   return (
     <>
-      <h2>This Home page </h2>
+      <h2>This Home page</h2>
       <Media></Media>
-      <Help></Help>
       <Footer></Footer>
     </>
   );
