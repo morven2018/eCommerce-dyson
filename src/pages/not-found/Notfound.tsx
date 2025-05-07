614f6df417c5d38fb3c3f6ef40089eafa6f39eed
morven2018 <you@example.com>
2025-05-07 14:03:22 +0300

    feat: add 404 content

--- src/pages/not-found/Notfound.tsx
+++ src/pages/not-found/Notfound.tsx
@@ -1,13 +1,24 @@
 import Footer from '../../components/layout/footer/Footer';
 import { Link } from 'react-router-dom';
 
+import styles from './notFound.module.scss';
+import { Button } from '@mui/material';
+
 export const NotFoundPage = () => {
   return (
     <>
-      <h2>Page 404</h2>
-      <div>
-        Go <Link to={'/'}>home</Link>
-      </div>
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <h2 className={styles.pageHeader}>
+            Oops! This page
+            <br /> doesn’t exist.
+          </h2>
+          <div className={styles.buttonWrapper}>
+            <Button className={styles.button}>home</Button>
+            <Button className={styles.button}>back</Button>
+          </div>
+        </div>
+      </main>
       <Footer></Footer>
     </>
   );
