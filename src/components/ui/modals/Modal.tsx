2a2c47d318d3ff51b3d60354677869ed68d3bf71
Ihar-Batura <A17331582@gmail.com>
2025-05-14 13:52:52 +0300

    feat: add component Modal

---
+++ src/components/ui/modals/Modal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from '@mui/material';
+
+interface ShowDialogProps {
+  message: string;
+}
+
+export default function ShowDialog({ message }: ShowDialogProps) {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
