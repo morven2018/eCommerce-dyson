a595ac708ebc4e847bb996bb4d794f5d242123cf
morven2018 <you@example.com>
2025-05-16 16:34:00 +0300

    feat: add help section to register page

--- src/pages/auth/register/Register.tsx
+++ src/pages/auth/register/Register.tsx
@@ -3,12 +3,14 @@ import Footer from '../../../components/layout/footer/Footer';
 import styles from './Register.module.scss';
 import { RegisterForm } from '../../../components/common/forms/register-form/RegisterForm';
 import { JSX } from 'react';
+import Help from '../../../components/layout/help/Help';
 
 export const RegisterPage = (): JSX.Element => {
   return (
     <>
       <h2 className={styles.pageHeader}>Register</h2>
-      <RegisterForm></RegisterForm>
+      <RegisterForm />
+      <Help />
       <Footer></Footer>
     </>
   );
