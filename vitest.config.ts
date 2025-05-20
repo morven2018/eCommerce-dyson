b8c61284f8f6973b0f93edaa7d4499d2d42cad42
Ihar-Batura <A17331582@gmail.com>
2025-05-20 17:08:36 +0300

    chore: add aliases to vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -14,6 +18,15 @@ export default defineConfig({
         branches: 25,
         statements: 25,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@shared': path.resolve(__dirname, './src/shared'),
     },
   },
 });
