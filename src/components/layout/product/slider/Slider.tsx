a3fcfb704c320c6ff401f4cc6cc14d274ff89e42
Ihar-Batura <A17331582@gmail.com>
2025-05-25 15:57:03 +0300

    feat: add component Slider

---
+++ src/components/layout/product/slider/Slider.tsx
@@ -0,0 +1,91 @@
+import styles from './Slider.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
+import 'swiper/css';
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+interface SliderProps {
+  images: Image[];
+}
+export function Slider({ images }: SliderProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const handleImageClick = (index: number) => {
+    setImageIndex(index);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handlePrevImage = () => {
+    setImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextImage = () => {
+    setImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <Swiper navigation={true} pagination={{ clickable: true }}>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={image.src}
+              alt={image.alt}
+              className={styles.image}
+              onClick={() => handleImageClick(index)}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {isModalOpen && (
+        <div className={styles.modalWindow}>
+          <div className={styles.modal}>
+            <button
+              className={styles.buttonCloseModal}
+              onClick={handleCloseModal}
+            >
+              X
+            </button>
+            <div className={styles.modalSlider}>
+              <img
+                src={images[imageIndex].src}
+                alt={images[imageIndex].alt}
+                className={styles.modalImage}
+              />
+            </div>
+            <div className={styles.arrowsContainer}>
+              <button
+                className={styles.arrow}
+                onClick={handlePrevImage}
+                disabled={images.length === 1}
+              >
+                ←
+              </button>
+              <button
+                className={styles.arrow}
+                onClick={handleNextImage}
+                disabled={images.length === 1}
+              >
+                →
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
