7195386d227d81ccb2f1844c407e6d34b6869ed1
morven2018 <you@example.com>
2025-05-27 04:27:48 +0300

    feat: add api for update first/last name

---
+++ src/shared/api/commerce-tools/updateFields/updateFirstName.ts
@@ -0,0 +1,62 @@
+import { Customer } from '@pages/profile/Profile';
+import { commercetoolsConfig } from '../config';
+
+interface UpdateCustomerData {
+  version: number;
+  actions: CustomerUpdateAction[];
+}
+interface CustomerUpdateAction {
+  action: string;
+  firstName?: string;
+}
+
+export async function updateFirstName(
+  newFirstName: string,
+  customerId: string,
+  version: number
+): Promise<Customer | null> {
+  const accessToken = localStorage.getItem('authDysonToken');
+
+  if (!accessToken) {
+    throw new Error('Authentication required');
+  }
+
+  const apiUrl = commercetoolsConfig.apiUrl;
+  const projectKey = commercetoolsConfig.projectKey;
+  const url = `${apiUrl}/${projectKey}/customers/${customerId}`;
+
+  const updateData: UpdateCustomerData = {
+    version,
+    actions: [
+      {
+        action: 'setFirstName',
+        firstName: newFirstName,
+      },
+    ],
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateData),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error('API Error:', errorData);
+      if (response.status === 401) {
+        localStorage.removeItem('authDysonToken');
+      }
+      throw new Error(response.status.toString());
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error updating customer last name:', error);
+    return null;
+  }
+}
