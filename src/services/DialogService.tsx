0b18dd926888e04724383da6a69a86ae10221dc7
morven2018 <you@example.com>
2025-06-01 20:24:29 +0300

    feat: add home button to modal

--- src/services/DialogService.tsx
+++ src/services/DialogService.tsx
@@ -1,13 +1,32 @@
 import ReactDOM, { Root } from 'react-dom/client';
 import ShowDialog from '@components/ui/modals/Modal';
+import { Button } from '@mui/material';
+import { ReactNode } from 'react';
 
-export const openDialog = (message: string) => {
+export const openDialog = (message: string, withHomeButton?: boolean) => {
   const container = document.createElement('div');
   document.body.appendChild(container);
 
+  const buttonToMain = (): ReactNode => (
+    <Button
+      onClick={() => {
+        window.location.href = '/';
+        cleanup(root, container);
+      }}
+    >
+      Home
+    </Button>
+  );
+
   const root = ReactDOM.createRoot(container);
   root.render(
-    <ShowDialog message={message} onClose={() => cleanup(root, container)} />
+    <>
+      <ShowDialog
+        message={message}
+        onClose={() => cleanup(root, container)}
+        additionalButton={withHomeButton && buttonToMain()}
+      />
+    </>
   );
 
   function cleanup(root: Root, container: HTMLElement) {
