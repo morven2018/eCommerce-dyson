25b8b8b55a9dbc91cdbc07cd2f0461978d5df169
morven2018 <you@example.com>
2025-06-03 23:47:07 +0300

    fix: remove double import

--- server.mjs
+++ server.mjs
@@ -1,7 +1,6 @@
-import express from 'express';
+import express, { dirname } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
