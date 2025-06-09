bbdc5c73f6417a1aa2d677707c047667dcf07043
Ihar-Batura <A17331582@gmail.com>
2025-06-09 11:44:24 +0300

    feat: add button to product card

--- src/components/ui/cards/Card.tsx
+++ src/components/ui/cards/Card.tsx
@@ -10,6 +10,15 @@ interface Card {
   src: string;
 }
 
+const handleAddToCart = (e: React.MouseEvent) => {
+  e.stopPropagation();
+  console.log('add product to cart');
+  console.log(e.target);
+  if (e.target instanceof HTMLButtonElement) {
+    e.target.classList.add('added');
+  }
+};
+
 export const Card = ({
   id,
   name,
@@ -20,7 +29,7 @@ export const Card = ({
 }: Card) => {
   const alt = 'Product picture';
   const maxProductNameLength = 30;
-  const maxProductDescriptionLength = 90;
+  const maxProductDescriptionLength = 75;
   const productName =
     name.length < maxProductNameLength
       ? name
@@ -32,37 +41,42 @@ export const Card = ({
       : `${description.slice(0, maxProductDescriptionLength)}...`;
 
   return (
-    <Link
-      to={`/product/${id}`}
-      className={styles.container}
-      onKeyDown={(e) => {
-        if (e.key === ' ') {
-          e.preventDefault();
-          e.currentTarget.click();
-        }
-      }}
-    >
-      <img src={src} alt={alt} className={styles.cardImage} />
-      <div className={styles.cardInfo}>
-        <div className={styles.cardInfoWrapper}>
-          <div className={styles.priceContainer}>
-            {discountedPrice && (
-              <span className={styles.discountedPrice}>
-                Price: ${(discountedPrice / 100).toFixed(2)}
+    <div className={styles.container}>
+      <Link
+        to={`/product/${id}`}
+        className={styles.linkContainer}
+        onKeyDown={(e) => {
+          if (e.key === ' ') {
+            e.preventDefault();
+            e.currentTarget.click();
+          }
+        }}
+      >
+        <img src={src} alt={alt} className={styles.cardImage} />
+        <div className={styles.cardInfo}>
+          <div className={styles.cardInfoWrapper}>
+            <div className={styles.priceContainer}>
+              {discountedPrice && (
+                <span className={styles.discountedPrice}>
+                  Price: ${(discountedPrice / 100).toFixed(2)}
+                </span>
+              )}
+              <span
+                className={`${styles.price} ${discountedPrice ? styles.strikethrough : ''}`}
+              >
+                {discountedPrice ? 'Initial price' : 'Price'} $
+                {(price / 100).toFixed(2)}
               </span>
-            )}
-            <span
-              className={`${styles.price} ${discountedPrice ? styles.strikethrough : ''}`}
-            >
-              {discountedPrice ? 'Initial price' : 'Price'} $
-              {(price / 100).toFixed(2)}
-            </span>
+            </div>
+            <span className={styles.name}>{productName}</span>
+            <span className={styles.line}></span>
           </div>
-          <span className={styles.name}>{productName}</span>
-          <span className={styles.line}></span>
+          <div className={styles.description}>{productDescription}</div>
         </div>
-        <div className={styles.description}>{productDescription}</div>
-      </div>
-    </Link>
+      </Link>
+      <button className={styles.button} onClick={handleAddToCart}>
+        Add to cart
+      </button>
+    </div>
   );
 };
