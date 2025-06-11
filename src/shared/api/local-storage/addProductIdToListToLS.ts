6acd761a3163c0e813473ae29a81d8bfee93fe3a
Ihar-Batura <A17331582@gmail.com>
2025-06-11 12:53:44 +0300

    refactor: delete two functions because change logic

--- src/shared/api/local-storage/addProductIdToListToLS.ts
+++
@@ -1,8 +0,0 @@
-export function addProductIdToListToLS(id: string): void {
-  const storedList = localStorage.getItem('listProductsIdInCart');
-  const list: string[] = storedList ? JSON.parse(storedList) : [];
-
-  list.push(id);
-
-  localStorage.setItem('listProductsIdInCart', JSON.stringify(list));
-}
