e2958fc6b60756f762b9e357f9da7eaeb08d451a
Ihar-Batura <A17331582@gmail.com>
2025-06-11 12:56:35 +0300

    feat: add function checkIfInCart

--- src/components/layout/product/card/ProductCard.tsx
+++ src/components/layout/product/card/ProductCard.tsx
@@ -1,6 +1,9 @@
 import styles from './ProductCard.module.scss';
 import Counter from '@components/ui/counter/Counter';
 import { Variants } from '../variants/Variants';
+import { apiGetCartById } from '@shared/api/commerce-tools/apiGetCartById';
+import { useState, useEffect } from 'react';
+import { openDialog } from '@services/DialogService';
 
 interface VariantsData {
   iconUrl: string;
@@ -8,6 +11,7 @@ interface VariantsData {
 }
 
 interface ProductCard {
+  id?: string;
   name: string;
   description: string;
   price: number;
@@ -16,12 +20,40 @@ interface ProductCard {
 }
 
 export default function ProductCard({
+  id,
   name,
   description,
   price,
   discountedPrice,
   variants,
 }: Readonly<ProductCard>) {
+  const [isProductInCart, setIsProductInCart] = useState(false);
+
+  useEffect(() => {
+    const checkIfInCart = async () => {
+      try {
+        const cart = await apiGetCartById();
+        if (!cart || !cart.lineItems) return;
+
+        const isInCart = cart.lineItems.some((item) => item.productId === id);
+        setIsProductInCart(isInCart);
+        console.log('isInCart', isInCart);
+      } catch (error) {
+        let message = 'Error get cart data';
+
+        if (error instanceof Error) {
+          message = error.message;
+        } else if (typeof error === 'string') {
+          message = error;
+        }
+
+        openDialog(message, true);
+      }
+    };
+
+    checkIfInCart();
+  }, [id]);
+
   return (
     <div className={styles.container}>
       <h3 className={styles.name}>{name}</h3>
@@ -33,7 +65,9 @@ export default function ProductCard({
         <div className={styles.initial}>Initial price: ${price.toFixed(2)}</div>
       )}
       <Counter price={discountedPrice ?? price} />
-      <button className={styles.button}>add to cart</button>
+      <button className={styles.button}>
+        {isProductInCart ? 'remove from cart' : 'add to cart'}
+      </button>
       <Variants variants={variants} />
     </div>
   );
